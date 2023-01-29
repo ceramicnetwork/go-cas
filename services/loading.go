55f7c70a5e82e99def24ae231c3dde9ec869adaf
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-01-29 17:16:22 -0500

    fix: resolve load requests for ceramic loading failures

--- services/loading.go
+++ services/loading.go
@@ -3,10 +3,12 @@ package services
 import (
 	"context"
 	"encoding/json"
-	"github.com/smrz2001/go-cas/services/ceramic"
 	"log"
 
+	"github.com/google/uuid"
+
 	"github.com/smrz2001/go-cas/models"
+	"github.com/smrz2001/go-cas/services/ceramic"
 )
 
 type LoadingService struct {
@@ -32,8 +34,11 @@ func NewLoadingService(
 // Load is the Loading service's message handler. It will be invoked for messages received on either the Load or the
 // Multiquery queues. The queue plumbing takes care of scaling the consumers, batching, etc.
 //
-// We won't return errors from here, which will cause the load request to be deleted from the originating queue. This is
-// ok because we'll re-attempt loading again several times anyway.
+// - Post an update to the Status queue and delete the request from the loading queue(s) if Ceramic loading succeeds,
+//   whether the CID is found or not.
+// - If a CID is not found during initial loading, post a new request to the Multiquery queue.
+// - For errors posting to SQS, just return the error. This will cause the request to not be deleted from the queue, and
+//   then to be reprocessed after the SQS visibility timeout.
 func (l LoadingService) Load(ctx context.Context, msgBody string) error {
 	anchorReq := new(models.AnchorRequestMessage)
 	if err := json.Unmarshal([]byte(msgBody), anchorReq); err != nil {
@@ -48,26 +53,32 @@ func (l LoadingService) Load(ctx context.Context, msgBody string) error {
 	}
 	if queryResult, err := l.ceramicLoader.Query(ctx, query); err != nil {
 		log.Printf("load: error loading query=%+v: %v", query, err)
-		return nil
+		// Post a failure to the Status queue and delete the request from the loading queue(s)
+		return l.resolve(ctx, query.Id, false)
 	} else if queryResult.CidFound {
-		// We found the CID!
-		if _, err = l.statusPublisher.SendMessage(ctx, models.RequestStatusMessage{Id: query.Id, Loaded: true}); err != nil {
-			return err
-		}
-	} else
-	// If we didn't find the CID we were looking for, send a multiquery to see if we can find newer commits.
-	if queryResult.StreamState != nil {
-		// Add the genesis CID and stream type to the request so that we can construct a CID-specific multiquery
+		// Post a successful update to the Status queue and delete the request from the loading queue(s)
+		return l.resolve(ctx, query.Id, true)
+	} else if queryResult.StreamState != nil {
+		// If the request to Ceramic was successful but we didn't find the CID we were looking for, send a multiquery to
+		// see if we can find newer commits. Add the genesis CID and stream type to the request so that we can construct
+		// a CID-specific multiquery.
 		anchorReq.GenesisCid = &queryResult.StreamState.Log[0].Cid
 		anchorReq.StreamType = &queryResult.StreamState.Type
 		if _, err = l.loadPublisher.SendMessage(ctx, anchorReq); err != nil {
+			// Return the error so that the request is not deleted from the Load queue and gets reprocessed after the
+			// SQS visibility timeout.
 			return err
 		}
 		// We can delete the event from the Load queue once a new event has been posted to the Multiquery queue
 		return nil
-	} else if _, err = l.statusPublisher.SendMessage(ctx, models.RequestStatusMessage{Id: query.Id}); err != nil {
-		return err
+	} else {
+		// We didn't find the CID even after a multiquery so post a failure to the Status queue and delete the request
+		// from the loading queue(s).
+		return l.resolve(ctx, query.Id, false)
 	}
-	// Delete the anchor request from the queue if we were able to resolve the stream status, whether found or not.
-	return nil
+}
+
+func (l LoadingService) resolve(ctx context.Context, id uuid.UUID, loaded bool) error {
+	_, err := l.statusPublisher.SendMessage(ctx, models.RequestStatusMessage{Id: id, Loaded: loaded})
+	return err
 }
