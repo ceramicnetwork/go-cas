03e5d876fd5b0940ebe55995763a344d95f96cf6
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-06-05 19:48:28 -0400

    chore: send auth token

--- ci/cmd/status/main.go
+++ ci/cmd/status/main.go
@@ -20,17 +20,24 @@ func main() {
 		GitHubToken string `arg:"env:GH_TOKEN" help:"GitHub auth token"`
 	}
 	arg.MustParse(&args)
-	if err := updateCommitStatus(args.Status, args.StatusUrl, args.RunUrl); err != nil {
+	if err := updateCommitStatus(args.Status, args.StatusUrl, args.RunUrl, args.GitHubToken); err != nil {
 		log.Fatalf("status: error publishing status [%v]", err)
 	}
 }
 
-func updateCommitStatus(status, statusUrl, targetUrl string) error {
+func updateCommitStatus(status, statusUrl, targetUrl, ghToken string) error {
 	reqBody, _ := json.Marshal(map[string]string{
 		"state":      status,
 		"target_url": targetUrl,
 	})
-	resp, err := http.Post(statusUrl, "application/vnd.github.v3+json", bytes.NewBuffer(reqBody))
+	req, err := http.NewRequest("POST", statusUrl, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", ghToken))
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
