25a7439d30752a9e36e7db2f3efd0fdb2b2b37d7
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-06-07 17:32:55 -0400

    fix: deploy to qa when deploying to dev

--- ci/cmd/deploy/main.go
+++ ci/cmd/deploy/main.go
@@ -23,6 +23,13 @@ const (
 	Env_EnvTag = "ENV_TAG"
 )
 
+const (
+	EnvTag_Dev  = "dev"
+	EnvTag_Qa   = "qa"
+	EnvTag_Tnet = "tnet"
+	EnvTag_Prod = "prod"
+)
+
 func main() {
 	ctx := context.Background()
 	if err := createJob(ctx); err != nil {
@@ -45,22 +52,29 @@ func createJob(ctx context.Context) error {
 	// Marshal the CD Manager job into DynamoDB-JSON, which is different from regular JSON and requires data types to be
 	// specified explicitly. The time encode function ensures that the timestamp has millisecond-resolution, which is
 	// what the CD Manager expects.
-	attributeValues, err := attributevalue.MarshalMapWithOptions(newJob, func(options *attributevalue.EncoderOptions) {
+	if attributeValues, err := attributevalue.MarshalMapWithOptions(newJob, func(options *attributevalue.EncoderOptions) {
 		options.EncodeTime = func(time time.Time) (types.AttributeValue, error) {
 			return &types.AttributeValueMemberN{Value: strconv.FormatInt(time.UnixMilli(), 10)}, nil
 		}
-	})
-	if err != nil {
+	}); err != nil {
+		return err
+	} else if awsCfg, err := config.AwsConfig(); err != nil {
 		return err
 	} else {
-		awsCfg, err := config.AwsConfig()
-		if err != nil {
-			return err
-		}
-		_, err = dynamodb.NewFromConfig(awsCfg).PutItem(ctx, &dynamodb.PutItemInput{
-			TableName: aws.String(fmt.Sprintf("ceramic-%s-ops", os.Getenv(Env_EnvTag))),
+		envTag := os.Getenv(Env_EnvTag)
+		client := dynamodb.NewFromConfig(awsCfg)
+		if _, err = client.PutItem(ctx, &dynamodb.PutItemInput{
+			TableName: aws.String(fmt.Sprintf("ceramic-%s-ops", envTag)),
 			Item:      attributeValues,
-		})
+		}); err != nil {
+			return err
+		} else if envTag == EnvTag_Dev {
+			// Also deploy to QA when deploying to Dev
+			_, err = client.PutItem(ctx, &dynamodb.PutItemInput{
+				TableName: aws.String(fmt.Sprintf("ceramic-%s-ops", EnvTag_Qa)),
+				Item:      attributeValues,
+			})
+		}
 		return err
 	}
 }
