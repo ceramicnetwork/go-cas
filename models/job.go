40d3bbcdf739f8e6f0144ac389eba1dfe5b2956d
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-06-07 15:40:10 -0400

    fix: add contract address to cd manager job (#11)

--- models/job.go
+++ models/job.go
@@ -8,3 +8,13 @@ const (
 	JobType_Deploy = "deploy"
 	JobType_Anchor = "anchor"
 )
+
+const (
+	JobParam_Id        = "id"
+	JobParam_Ts        = "ts"
+	JobParam_Stage     = "stage"
+	JobParam_Type      = "type"
+	JobParam_Params    = "params"
+	JobParams_Version  = "version"
+	JobParams_Contract = "contract"
+)
