8197f925c4380d0ac0aa31106313ba9538fcb010
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-07-11 16:54:39 -0400

    feat: prevent batch starvation (#18)

--- common/aws/queue/consumer.go
+++ common/aws/queue/consumer.go
@@ -2,23 +2,37 @@ package queue
 
 import (
 	"log"
+	"math"
 
 	"github.com/abevier/go-sqs/gosqs"
 )
 
-const consumerMaxWorkers = 100
-const consumerMaxReceiveMessageRequests = 12
+const defaultConsumerMaxWorkers = 100
+
+type ConsumerOpts struct {
+	MaxReceivedMessages int
+	MaxWorkers          int
+	MaxInflightRequests int
+}
 
 type Consumer struct {
 	queueType QueueType
 	consumer  *gosqs.SQSConsumer
 }
 
-func NewConsumer(publisher *Publisher, callback gosqs.MessageCallbackFunc) *Consumer {
+func NewConsumer(publisher *Publisher, callback gosqs.MessageCallbackFunc, opts *ConsumerOpts) *Consumer {
+	maxWorkers := defaultConsumerMaxWorkers
+	maxReceivedMessages := math.Ceil(float64(maxWorkers) * 1.2)
+	maxInflightRequests := math.Ceil(maxReceivedMessages / 10)
+	if opts != nil {
+		maxReceivedMessages = float64(opts.MaxReceivedMessages)
+		maxWorkers = opts.MaxWorkers
+		maxInflightRequests = float64(opts.MaxInflightRequests)
+	}
 	qOpts := gosqs.Opts{
-		MaxReceivedMessages:               consumerMaxWorkers * 1.2,
-		MaxWorkers:                        consumerMaxWorkers,
-		MaxInflightReceiveMessageRequests: consumerMaxReceiveMessageRequests,
+		MaxReceivedMessages:               int(maxReceivedMessages),
+		MaxWorkers:                        maxWorkers,
+		MaxInflightReceiveMessageRequests: int(maxInflightRequests),
 	}
 	return &Consumer{publisher.queueType, gosqs.NewConsumer(qOpts, publisher.publisher, callback)}
 }
