5a9519c726f6759ab195db8907ca17b21bc76aee
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-09-11 17:51:11 -0400

    fix: publish contract image + e2e test fixes

---
+++ contracts/publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Build and publish a docker image to a public ECR repo
+#
+# DOCKER_PASSWORD must be set
+# Use:
+#
+#   export DOCKER_PASSWORD=$(aws ecr-public get-login-password --region us-east-1)
+#   echo "${DOCKER_PASSWORD}" | docker login --username AWS --password-stdin public.ecr.aws/r5b3e0r5
+#
+# to login to docker. That password will be valid for 12h.
+
+docker buildx build --load -t 3box/cas-contract .
+docker tag 3box/cas-contract:latest public.ecr.aws/r5b3e0r5/3box/cas-contract:latest
+docker push public.ecr.aws/r5b3e0r5/3box/cas-contract:latest
