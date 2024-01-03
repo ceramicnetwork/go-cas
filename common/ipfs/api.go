868bf77832016545ed7b95e31ee404fd8cd36c8c
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2024-01-03 14:13:38 -0800

    Add error to ipfs error log

--- common/ipfs/api.go
+++ common/ipfs/api.go
@@ -87,7 +87,7 @@ func (i *IpfsApi) processIpfsTask(ctx context.Context, task any) error {
 
 func (i *IpfsApi) limiterRunFunction(ctx context.Context, task any) (any, error) {
 	if err := i.processIpfsTask(ctx, task); err != nil {
-		i.logger.Errorf("IPFS error for task %v on ipfs %v", task, i.multiAddressStr)
+		i.logger.Errorf("IPFS error for task %v on ipfs %v: %w", task, i.multiAddressStr, err)
 		i.metricService.Count(ctx, models.MetricName_IpfsError, 1)
 		// TODO: if we get many timeout errors, adjust the limiter values
 		// TODO: restart the ipfs instance if there are too many errors and mark unavailable
